c6d8bbca5b9df05c5078ad6ead7189a4a6379091
Conrad Irwin <conrad.irwin@gmail.com>
2012-08-11 03:01:06 -0700

    Document and split out into two files

--- lib/interception.rb
+++ lib/interception.rb
@@ -1,16 +1,66 @@
 require 'thread'
 
+
+# Provides global facility for monitoring exceptions raised in your application.
 module Interception
 
   class << self
     attr_accessor :mutex, :listeners, :rescueing
   end
-
   self.mutex = Mutex.new
   self.listeners = []
+  self.rescueing = false
 
+  # Listen for any exceptions raised.
+  #
+  # The listener block that you pass in will be executed as though inside Kernel#raise,
+  # so your entire program is still actively running. If you have a gem like
+  # pry-stack_explorer you can access the stack frames that lead to the exception
+  # occurring.
+  #
+  # NOTE: Be careful when writing a listener, if your listener raises an
+  #       exception it will mask the original exception.
+  #
+  # @example
+  #
+  #   # To report exceptions for the entire run of the program:
+  #   Interception.listen do |exception, binding|
+  #     Emailer.spam!('on-duty@startup.com', exception, binding.eval('self.class.name'))
+  #   end
+  #
+  # @example
+  #
+  #   # To log exceptions for the duration of a given block.
+  #   def log_exceptions(&block)
+  #     Interception.listen(block) do |exception, binding|
+  #       puts "#{binding.eval("self.inspect")} raised #{exception.inspect}"
+  #     end
+  #   end
+  #
+  # @example
+  #
+  #   # You can also turn listeners on and off manually
+  #
+  #   listener = Proc.new{ |exception, binding|
+  #     binding.pry
+  #   }
+  #   Interception.listen(listener)
+  #   Async::Redis.get("foo") do
+  #     Interception.unlisten(listener)
+  #   end
+  #
+  # @param [Proc] for_block (nil) If you pass for_block in, then you will only
+  #                               intercept exceptions raised while that block
+  #                               is running.
+  # @param [Proc] listen_block    The block to call when an exception occurs,
+  #                               takes two arguments, the exception and the
+  #                               binding
+  # @return [Object]              The return value of the for_block (if present)
+  # @yield [Exception]
+  # @yield [Binding]
+  # @see {unlisten}
   def self.listen(for_block=nil, &listen_block)
-    raise "no block given" unless listen_block || for_block
+    raise ArgumentError, "no block given" unless listen_block || for_block
     mutex.synchronize{
       start if listeners.empty?
       listeners << listen_block || for_block
@@ -22,9 +72,14 @@ module Interception
       ensure
         unlisten listen_block
       end
+    else
+      listen_block
     end
   end
 
+  # Disable a previously added listener
+  #
+  # @param [Proc] listen_block  The listen block you wish to remove.
   def self.unlisten(listen_block)
     mutex.synchronize{
       listeners.delete listen_block
@@ -32,6 +87,16 @@ module Interception
     }
   end
 
+  # Called by platform-specific implementations whenever an exception is raised.
+  #
+  # The arguments will be forwarded on to all listeners added via {listen} that
+  # haven't been removed via {unlisten}.
+  #
+  # For efficiency, this block will never be called unless there are active
+  # listeners.
+  #
+  # @param [Exception] e  The exception that was raised
+  # @param [Binding] binding  The binding from which it was raised
   def self.rescue(e, binding)
     return if rescueing
     self.rescueing = true
@@ -42,52 +107,13 @@ module Interception
     self.rescueing = false
   end
 
-  if defined? Rubinius
-    def self.start
-      class << Rubinius
-        alias raise_with_no_interception raise_exception
+  # Start sending events to rescue.
+  # @see cross_platform.rb
+  def self.start; raise NotImplementedError end
 
-        def raise_exception(exc)
-          bt = Rubinius::VM.backtrace(1, true).drop_while do |x|
-            x.variables.method.file.to_s.start_with?("kernel/")
-          end.first
-          b = Binding.setup(bt.variables, bt.variables.method, bt.constant_scope, bt.variables.self, bt)
+  # Stop sending events to rescue.
+  # @see cross_platform.rb
+  def self.stop; raise NotImplementedError end
 
-          Interception.rescue(exc, b)
-          raise_with_no_interception(exc)
-        end
-      end
-    end
-
-    def self.stop
-      class << Rubinius
-        alias raise_exception raise_with_no_interception
-      end
-    end
-  elsif defined?(JRuby)
-    require 'java'
-    $CLASSPATH << File.expand_path('../../ext/', __FILE__)
-    java_import org.pryrepl.InterceptionEventHook
-
-    def self.start
-      old_verbose = $VERBOSE
-      $VERBOSE = nil
-      JRuby.runtime.add_event_hook(hook)
-    ensure
-      $VERBOSE  = old_verbose
-    end
-
-    def self.stop
-      JRuby.runtime.remove_event_hook(hook)
-    end
-
-    def self.hook
-      @hook ||= InterceptionEventHook.new(proc do |e, b|
-        self.rescue(e, b)
-      end)
-    end
-
-  else
-    require File.expand_path('../../ext/interception.so', __FILE__)
-  end
+  require File.expand_path('../cross_platform.rb', __FILE__)
 end
