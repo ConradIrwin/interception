e9ce3e04c5cde82c2bb16754243354cee15d2b7f
GOTOH, Yutaka <ygotoh@ygotoh.jp>
2014-02-27 03:45:09 +0900

    lib/interception.rb: methods depending on platform move to cross_platform.rb

--- lib/cross_platform.rb
+++ lib/cross_platform.rb
@@ -59,9 +59,26 @@ class << Interception
       end)
     end
 
-  else #MRI
+  # For MRI
+  # @note For Ruby 2.1 and later we use the new TracePoint API.
+  elsif RUBY_VERSION.to_f >= 2.1 && RUBY_ENGINE == 'ruby'
 
-    require File.expand_path('../../ext/interception', __FILE__) unless Interception.ruby_21?
+    def start
+      @tracepoint ||= TracePoint.new(:raise) do |tp|
+        self.rescue(tp.raised_exception, tp.binding)
+      end
+
+      @tracepoint.enable
+    end
+
+    def stop
+      @tracepoint.disable
+    end
+
+  # For old MRI
+  else
+
+    require File.expand_path('../../ext/interception', __FILE__)
 
   end
 end
--- lib/interception.rb
+++ lib/interception.rb
@@ -108,34 +108,5 @@ module Interception
     self.rescueing = false
   end
 
-  # Start sending events to rescue.
-  # Implemented per-platform
-  # @note For Ruby 2.1 we use the new TracePoint API.
-  def self.start
-    if ruby_21?
-      @tracepoint ||= TracePoint.new(:raise) do |tp|
-        self.rescue(tp.raised_exception, tp.binding)
-      end
-
-      @tracepoint.enable
-    else
-      raise NotImplementedError
-    end
-  end
-
-  # Stop sending events to rescue.
-  # Implemented per-platform
-  def self.stop
-    if ruby_21?
-      @tracepoint.disable
-    else
-      raise NotImplementedError
-    end
-  end
-
-  def self.ruby_21?
-    RUBY_VERSION.to_f == 2.1 && RUBY_ENGINE == 'ruby'
-  end
-
   require File.expand_path('../cross_platform.rb', __FILE__)
 end
